37e86763c285451a0332c567e412afca385deba2
aviadhager <aviadhager@gmail.com>
2019-11-14 12:44:54 +0200

    Make the router works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,    AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
   bootstrap:    [ AppComponent ],
   providers: [
