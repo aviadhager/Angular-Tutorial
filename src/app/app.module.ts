a6a2140ae42e90ff9a1dd49e0712806b597bb747
aviadhager <aviadhager@gmail.com>
2019-12-04 16:14:27 +0200

    Add the dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { MessagesComponent } from './messages/messages.component';
 
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,    AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
