60ebb75d79e9a94872c8421a4d600e701be97e88
aviadhager <aviadhager@gmail.com>
2019-12-15 13:57:40 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,12 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,    AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule,    AppRoutingModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ],
   providers: [
